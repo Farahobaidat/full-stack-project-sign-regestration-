43d49cb1768b2ad714f3f7b852c577dc5d42bb43
ammarsaeed-beejo <ammarsaeed.art@gmail.com>
2021-08-04 19:56:41 +0300

    add new pages

---
+++ Register/script.js
@@ -0,0 +1,13 @@
+
+document.getElementById("UserName").onclick = function () { UserName() };
+
+function UserName() {
+    document.getElementById("UserNamealert").innerHTML = "please enter username";
+}
+
+document.getElementById("Useremail").onclick = function () { Useremail() };
+
+function Useremail() {
+    document.getElementById("Useremailalert").innerHTML = "please enter email";
+}
+
